b15a7c38fdb79dbd9d81b07836b8156bdb78e9cf
Gwen <g@10degres.net>
2018-01-29 12:21:00 +0100

    quick run

---
+++ quick-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage: "$0" <url file> [<payloads file>]"
+    if [ -n "$1" ] ; then
+	echo "Error: "$1"!"
+    fi
+    exit
+}
+
+if [ $# -lt 1 ] || [ $# -gt 2 ] ; then
+    usage
+fi
+
+f_src=$1
+
+if [ $# -eq 2 ] ; then
+	f_payloads=$2
+else
+	f_payloads=brute-full.txt
+fi
+
+phantom=$(whereis phantomjs | awk '{print $2}')
+
+php testxss.php --urls=$f_src --threads=10 --payload=$f_payloads --prefix --suffix --gpg --phantom=$phantom --sos --verbose=2
+
